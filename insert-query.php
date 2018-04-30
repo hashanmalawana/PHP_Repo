6d58c9f401a96a1fd749a2d30508a48e7e30f9e4
Hashan Malawana <hashanmalawana@gmail.com>
2018-04-30 23:54:57 +0530

    Php87

--- insert-query.php
+++ insert-query.php
@@ -8,19 +8,8 @@
  $password = 'mypass';
  $is_deleted = 0;
 
- $hashed_password = sha1($password); 
-
-$query = "INSERT INTO user (first_name, last_name, email, password,is_deleted) VALUES ('{$first_name}', '{$last_name}', '{$email}', '{$password}', {$is_deleted})";
-
-$result = mysql_query($connection, $query);
-
-
-if($result){
-	echo "Hari";
-}
-else{
-	echo "Weradi";
-}
+ $hashed_password = sha1($password);
+ echo "Hashed password : {$hashed_password}";
 
  ?>
 
